06997f94c7beb8f7396dd66fd0c15af694e0280f
anthonygavriel <anthonygavriel@gmail.com>
2022-10-05 13:47:44 +0100

    API connection

---
+++ frontend/src/API.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+export default axios.create({
+    baseURL: 'http://3.11.212.147:8000/',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+})
