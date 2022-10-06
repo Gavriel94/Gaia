7df60fca556fc1ba6b5a7cab1f8fc8ea15df4168
anthonygavriel <anthonygavriel@gmail.com>
2022-10-06 15:17:40 +0100

    IP change

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://3.11.212.147:8000/',
+    baseURL: 'http://13.42.72.126:8000/',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
