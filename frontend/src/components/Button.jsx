9c2759286acc72f06b73cdc158b478d6955b4526
anthonygavriel <anthonygavriel@gmail.com>
2022-10-15 12:53:58 +0100

    added label styling

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 /**
  * Button component which displays a text label and can perform functions when the prop is passed
  * 
- * @param label - Text displayed on the button
- * @param func - Allows the button to perform any function defined in the top level page
- * @param icon - Displays an icon, usually used with react-icons
- * @returns Functional button with the consistent styling of Project Gaia
+ * @param label Text displayed on the button
+ * @param labelProps Changes in text styling if required
+ * @param func Allows the button to perform any function defined in the top level page
+ * @param icon Displays an icon, usually used with react-icons
+ * @param image Displays an image in the button
+ * @param imageAlt alt text required for images
+ * @param imageWidth 
+ * @param imageHeight
+ * @returns Functional button with the consistent styling of Gaia
  */
 
-const Button = ({ label, func, icon, image, imageAlt, imageWidth, imageHeight }) => {
+const Button = ({ label, labelProps, func, icon, image, imageAlt, imageWidth, imageHeight }) => {
     return (
         <button
             type='button'
@@ -17,10 +22,12 @@ const Button = ({ label, func, icon, image, imageAlt, imageWidth, imageHeight })
             className='rounded-full focus:outline-none
         bg-light-orange hover:bg-light-white hover:text-light-orange text-light-white
         dark:bg-dark-orange dark:hover:bg-dark-grey dark:hover:text-dark-orange dark:text-white py-2 px-4 text-xl font-bold transition-color duration-500 cursor-pointer'>
-            {<img src={image} alt={imageAlt} height={imageHeight} width={imageWidth} />}
             <div className='flex flex-row text-center justify-center'>
+            {<img src={image} alt={imageAlt} height={imageHeight} width={imageWidth} />}
                 {icon}
-                {label}
+                <div className={`${labelProps}`}>
+                    {label}
+                </div>
             </div>
         </button>
     )
