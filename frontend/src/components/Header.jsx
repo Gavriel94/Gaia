38b3c0f48a967f74348f2bc9bade3b44079b5e85
anthonygavriel <anthonygavriel@gmail.com>
2022-10-18 10:20:49 +0100

    header prop added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { BsSun, BsMoon } from 'react-icons/bs'
 import Button from './Button'
 import { useStateContext } from '../context/ContextProvider'
 import { BiTrendingUp } from 'react-icons/bi'
 import WalletConnectV2 from './WalletConnectV2'
+import SortingButton from './SortingButton'
 
 
 /**
@@ -13,15 +14,18 @@ import WalletConnectV2 from './WalletConnectV2'
  * Contains a button for wallet connectivity and another to switch dark/light mode 
  */
 
-const Header = () => {
-    const { darkMode, setDarkMode } = useStateContext();
+const Header = ({ page }) => {
+    const { darkMode, setDarkMode, showLogoutAlert, showErrorAlert } = useStateContext();
 
     return (
         <div>
             <div className='flex items-end flex-col'>
-                <div className='flex min-w-full sm:w-1/2 items-center justify-center bottom-0 sm:right-3 sm:justify-end sm:top-0 sm:h-16 fixed sm:px-20 sm:py-1 bg-white dark:bg-dark-grey opacity-100 sm:bg-opacity-0'>
+                <div className='flex min-w-full sm:w-1/2 items-center justify-center bottom-0 sm:left-24 sm:justify-end sm:top-0 sm:h-16 fixed sm:px-20 sm:py-1 bg-white dark:bg-dark-grey opacity-100 sm:bg-opacity-0'>
                     <div className='flex flex-row pr-10'>
-                        <div className='px-2 py-3'>
+                        <div className={`${page === 'home' ? 'block' : 'hidden'} ${page === 'landing' && 'hidden'}`}>
+                            <SortingButton />
+                        </div>
+                        <div className={`${page === 'landing' && 'hidden'} px-4 py-3`}>
                             <WalletConnectV2 />
                         </div>
                         <div className={`py-3 px-2 sm:hidden`}>
@@ -32,7 +36,13 @@ const Header = () => {
                                 />
                             </Link>
                         </div>
-                        <div className='py-3 pl-2'>
+                        <div className={`${page !== 'landing' && 'hidden'} py-3 px-2`}>
+                            <Link to='/home'>
+                                <Button title={'Enter App'} icon={'Enter App'} />
+                            </Link>
+                        </div>
+
+                        <div className={`${showLogoutAlert && 'hidden'} ${showErrorAlert && 'hidden'} py-3`}>
                             <Button
                                 title={'mode-toggle'}
                                 func={() => {
@@ -41,6 +51,7 @@ const Header = () => {
                                 icon={darkMode === false ? <BsMoon size={'26px'} /> : <BsSun size={'26px'} />}
                             />
                         </div>
+
                     </div>
                 </div>
             </div>
