732760db760c4e0a02319a95cc5029c16b777582
anthonygavriel <anthonygavriel@gmail.com>
2022-10-19 01:10:17 +0100

    styling fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,6 +8,7 @@ import WalletConnectV2 from './WalletConnectV2'
 import SortingButton from './SortingButton'
 
 
+
 /**
  * Header component to be displayed on Home page
  * Contains a button to sort the articles and a shortcut to Create
@@ -20,7 +21,7 @@ const Header = ({ page }) => {
     return (
         <div>
             <div className='flex items-end flex-col'>
-                <div className='flex min-w-full sm:w-1/2 items-center justify-center bottom-0 sm:left-24 sm:justify-end sm:top-0 sm:h-16 fixed sm:px-20 sm:py-1 bg-white dark:bg-dark-grey opacity-100 sm:bg-opacity-0'>
+                <div className='flex min-w-full sm:w-1/2 items-center justify-center bottom-0 sm:left-24 sm:justify-end sm:top-0 sm:h-16 fixed sm:px-20 sm:py-1 opacity-100 sm:bg-opacity-0'>
                     <div className='flex flex-row pr-10'>
                         <div className={`${page === 'home' ? 'block' : 'hidden'} ${page === 'landing' && 'hidden'}`}>
                             <SortingButton />
@@ -38,11 +39,18 @@ const Header = ({ page }) => {
                         </div>
                         <div className={`${page !== 'landing' && 'hidden'} py-3 px-2`}>
                             <Link to='/home'>
-                                <Button title={'Enter App'} icon={'Enter App'} />
+                                <button
+                                    type='button'
+                                    className='rounded-full
+                                        bg-black text-light-white dark:hover:text-dark-orange dark:text-white py-2 px-4 text-xl font-bold transition-color duration-500 cursor-pointer'>
+                                    <div className='flex flex-row text-center justify-center'>
+                                        <div>Skip</div>
+                                    </div>
+                                </button>
                             </Link>
                         </div>
 
-                        <div className={`${showLogoutAlert && 'hidden'} ${showErrorAlert && 'hidden'} py-3`}>
+                        <div className={`${showLogoutAlert && 'hidden'} ${showErrorAlert && 'hidden'} ${page === 'landing' && 'hidden'} py-3`}>
                             <Button
                                 title={'mode-toggle'}
                                 func={() => {
