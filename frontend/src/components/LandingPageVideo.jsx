743ba703a955666c64d1872b085a1d797dda2246
anthonygavriel <anthonygavriel@gmail.com>
2022-10-19 02:21:39 +0100

    video changes

--- frontend/src/components/LandingPageVideo.jsx
+++ frontend/src/components/LandingPageVideo.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
-import Button from './Button'
-import GaiaAnim from '../assets/GaiaAnimPrototype.mp4'
+import GaiaAnim from '../assets/GaiaAnimCompressed.mp4'
 import '../video.css'
 import { CgEnter } from 'react-icons/cg'
 
