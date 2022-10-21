a5c7997ce564c5844b11362097593bc4b50f0196
anthonygavriel <anthonygavriel@gmail.com>
2022-10-21 08:59:30 +0100

    submitting svg added

--- frontend/src/pages/CreateArticleV2.jsx
+++ frontend/src/pages/CreateArticleV2.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { Header, Sidebar, Title, InputField, Button, Editor } from '../components'
+import { Header, Sidebar, Title, InputField, Button, Editor, LoadingSpinner } from '../components'
 import API from '../API'
 
 const CreateArticleV2 = () => {
@@ -8,6 +8,21 @@ const CreateArticleV2 = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [tags, setTags] = useState('')
+  const [submit, setSubmit] = useState(false)
+  const [submitted, setSubmitted] = useState(false)
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async function confirmSubmit() {
+    setSubmit(true);
+    await sleep(2000);
+    setSubmitted(true)
+    await sleep(1000);
+    setSubmitted(false)
+    setSubmit(false);
+  }
 
   const handleTitle = (e) => {
     setTitle(e)
@@ -26,45 +41,68 @@ const CreateArticleV2 = () => {
       content: content,
       tags: tags,
     }
-
-    API.post("/create/", { ...article });
+    try {
+      API.post("/create/", { ...article });
+      confirmSubmit()
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   // write a condition, if content is empty don't submit
   return (
     <>
-
-      <div className='fixed justify-center m-auto left-0 right-0 '>
-        <Header />
-        <Sidebar />
-      </div>
-      <div className='flex justify-center'>
-        <div className='pt-20'>
-          <Title text={'Create'} size={'text-6xl'} />
-          <div className='mt-20'>
-            <InputField
-              required={true}
-              type='input'
-              placeholder='Title'
-              defaultValue={''}
-              onChange={e => handleTitle(e.target.value)}
-            />
-            <div className='py-5' />
-            <Editor setContent={setContent} />
-            <div className='py-5' />
-            <InputField
-              required={true}
-              type='input'
-              placeholder='Tags'
-              defaultValue={''}
-              onChange={e => handleTags(e.target.value)}
-            />
-            <div className='py-5' />
-            <div className='flex justify-center'>
-              <Button label={'Submit'} func={handleSubmit} />
+      {
+        submit && (
+          <>
+            <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pt-40'>
+              <div className={`${submitted && 'hidden'}`}>
+                <LoadingSpinner />
+              </div>
+              <div className={`${submitted && 'hidden'} mt-10`}>
+                <Title text={'Submitting'} size={'text-3xl'} />
+              </div>
+              <div className={`${!submitted && 'hidden'}`}>
+                <Title text={'Submitted!'} size={'text-6xl'} />
+              </div>
             </div>
-            <div className='flex justify-center'>
-              <p className="input-center mt-2 input-sm input- input-dark-grey dark:input-light-white">Unsure how to start? Read the full <Link to="/articleguide" className="font-medium text-light-orange dark:text-dark-orange hover:underline">Article Guide</Link>.</p>
+
+          </>
+        )
+      }
+      <div className={`${submit ? 'hidden' : 'block'}`}>
+        <div className='fixed justify-center m-auto left-0 right-0 '>
+          <Header />
+          <Sidebar />
+        </div>
+        <div className='flex justify-center'>
+          <div className='pt-20'>
+            <Title text={'Create'} size={'text-6xl'} />
+            <div className='mt-20'>
+              <InputField
+                required={true}
+                type='input'
+                placeholder='Title'
+                defaultValue={''}
+                onChange={e => handleTitle(e.target.value)}
+              />
+              <div className='py-5' />
+              <Editor setContent={setContent} />
+              <div className='py-5' />
+              <InputField
+                required={true}
+                type='input'
+                placeholder='Tags'
+                defaultValue={''}
+                onChange={e => handleTags(e.target.value)}
+              />
+              <div className='py-5' />
+              <div className='flex justify-center'>
+                <Button label={'Submit'} func={handleSubmit} />
+              </div>
+              <div className='flex justify-center'>
+                <p className='text-center mt-2 text-black dark:text-white'>Unsure how to start? Read the full <Link to="/articleguide" className="font-medium text-light-orange dark:text-dark-orange" style={{ textDecoration: 'none' }}>Article Guide</Link>.</p>
+              </div>
             </div>
           </div>
         </div>
