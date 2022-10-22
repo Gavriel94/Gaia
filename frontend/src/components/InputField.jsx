96830f96c6277e0b9591e094bac659057ca0b152
anthonygavriel <anthonygavriel@gmail.com>
2022-10-22 16:59:53 +0100

    more customisable options

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const InputField = ({ required, type, placeholder, defaultValue, onChange }) => {
+/**
+ * Custom input field 
+ * borderColor must be defined as the whole Tailwind class string 'border-x'
+ */
+
+const InputField = ({ required, type, placeholder, defaultValue, onChange, borderColor }) => {
     return (
-        <div className='border-2 border-light-orange rounded-lg'>
+
+        <div className={`border-4 ${borderColor === undefined ? 'border-light-orange' : borderColor} rounded-lg`}>
             <input className='py-3 grid-cols-2 rounded w-full px-4
                 focus:outline-none'
                 required={required}
