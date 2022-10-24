5d81c46e73d156fb0f23f94c41aa9fa08cacc950
anthonygavriel <anthonygavriel@gmail.com>
2022-10-24 21:30:45 +0100

    new backend server using postgres

--- frontend/src/API.js
+++ frontend/src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://13.42.72.126:8000/',
+    baseURL: 'http://18.130.61.179:8000/',
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
