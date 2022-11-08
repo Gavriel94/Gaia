3ddac989914e7d48192e081eeb39c0aee1df0959
anthonygavriel <anthonygavriel@gmail.com>
2022-11-08 22:27:20 +0000

    Reorganised component

--- frontend/src/components/article/MiniArticle.jsx
+++ frontend/src/components/article/MiniArticle.jsx
@@ -27,7 +27,7 @@ const MiniArticle = ({ id, title, content, tags, image }) => {
      */
     const contentPreview = () => {
         if (content.length > 120) {
-            let reducedContent = content.slice(0, 120) + '...'
+            let reducedContent = content.slice(0, 119) + '...'
             return parser(reducedContent)
         }
         return parser(content)
@@ -38,20 +38,18 @@ const MiniArticle = ({ id, title, content, tags, image }) => {
             <div>
                 <div className='border-light-orange dark:border-dark-orange p-6 mb-3 w-[250px] border-b-1 md:w-[500px] lg:w-[700px] sm:border-1 sm:border-opacity-50 sm:rounded-3xl'>
                     <Link to={`/articles/${id}`} style={{ textDecoration: 'none' }}>
-                        <div className='flex'>
-                            <div className='w-2/3'>
-                                <div>
-                                    <Title text={title} size={'text-3xl'} />
-                                </div>
-                                <div className='mt-10 text-center text-black dark:text-light-white'>
-                                    {contentPreview()}
-                                </div>
-                                <div className='mt-4 text-black dark:text-light-white'>
-                                    {tags}
-                                </div>
+                        <div className='grid grid-cols-2 grid-rows-2 justify-end content-end'>
+                            <div className=''>
+                                <Title text={title} size={'text-3xl'} />
                             </div>
-                            <div className='justify-end sm:m-auto pl-5'>
-                                <img src={`${image}`} alt="" height={'150'} width={'150'} className='rounded-lg' />
+                            <div className="justify-end pl-20 hidden sm:block">
+                                <img src={`${image}`} alt='Preview' height={'150'} width={'150'} className='rounded-lg' />
+                            </div>
+                            <div className="justify-end pl-5 sm:hidden grid-cols-1">
+                                <img src={`${image}`} alt='Preview' height={'150'} width={'150'} className='rounded-lg' />
+                            </div>
+                            <div className='col-span-2 text-center dark:text-white text-black mt-10'>
+                                {contentPreview()}
                             </div>
                         </div>
                     </Link>
