95b88c2d6b19c446ed8832ff9c88edcaa8648067
anthonygavriel <anthonygavriel@gmail.com>
2022-11-08 22:33:28 +0000

    Removes permission required from backend

--- frontend/src/pages/article/CreateArticleV2.jsx
+++ frontend/src/pages/article/CreateArticleV2.jsx
@@ -15,7 +15,7 @@ import { MdOutlineCancel } from 'react-icons/md'
 
 const CreateArticleV2 = () => {
 
-  const { darkMode, loggedInProfile, sessionToken, submitted, setSubmitted } = useStateContext()
+  const { darkMode, loggedInProfile, sessionToken, submitted, adaHandleDetected, adaHandleSelected } = useStateContext()
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
 
@@ -95,27 +95,22 @@ const CreateArticleV2 = () => {
     setPreviewImage(e.target.files[0])
   }
 
-  const createForm = async () => {
-    let newArticle = new FormData()
-    newArticle.append('title', title)
-    newArticle.append('content', content)
-    newArticle.append('tags', tags)
-    newArticle.append('preview_image', previewImage)
-    newArticle.append('author', loggedInProfile.username)
-    return newArticle
-  }
-
   async function handleSubmit() {
     let newArticle = new FormData()
     newArticle.append('title', title)
     newArticle.append('content', content)
     newArticle.append('tags', tags)
     newArticle.append('preview_image', previewImage)
-    newArticle.append('author', loggedInProfile.username)
-
+    newArticle.append('author', loggedInProfile.id)
+    if(adaHandleDetected) {
+      newArticle.append('author_display_name', adaHandleSelected)
+    }
+    else { 
+      newArticle.append('author_display_name', loggedInProfile.username)
+    }
     console.log(newArticle)
     console.log(sessionToken)
-    console.log(loggedInProfile.username)
+    console.log(loggedInProfile)
     for (const v of newArticle.values()) {
       console.log(v)
     }
@@ -123,10 +118,11 @@ const CreateArticleV2 = () => {
       setSubmit(true)
       var id = await API.post("articles/article/create/", newArticle, {
         headers: {
-          'Authorization': `Token ${sessionToken}`,
+          // 'Authorization': `Token ${sessionToken}`,
           'Content-Type': 'multipart/form-data',
         },
-      }).then(response => id = response.data.id)
+      }
+      ).then(response => id = response.data.id)
       setID(id)
       setSubmit(false)
       setIDSet(true)
@@ -140,7 +136,7 @@ const CreateArticleV2 = () => {
 
   return (
     <>
-          {
+      {
         submit && (
           <>
             <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pt-40'>
@@ -177,112 +173,112 @@ const CreateArticleV2 = () => {
           </>
   )
 } */}
-{
-  <div className={`${submit ? 'hidden' : 'block'}`}>
-    <div className='fixed justify-center m-auto left-0 right-0 '>
-      <Header />
-      <Sidebar />
-    </div>
-    <div>
-      <div className={`${!sessionToken ? 'flex justify-center text-white ' : 'hidden'}`}>
-        <div className='mt-20'>
-          You must login to post an Article
-          <div className='flex justify-center mt-20'>
-            <LoginButton />
+      {
+        <div className={`${submit ? 'hidden' : 'block'}`}>
+          <div className='fixed justify-center m-auto left-0 right-0 '>
+            <Header />
+            <Sidebar />
           </div>
-        </div>
-      </div>
-    </div>
-    <div className={`${!sessionToken ? 'hidden' : 'block'}`}>
-      <div className='flex justify-center'>
-        <div className='pt-20'>
-          <Title text={title} size={'text-6xl'} />
-          <div className='flex justify-center mt-20'>
-            <div className='rounded-full focus:outline-none
+          <div>
+            <div className={`${!sessionToken ? 'flex justify-center dark:text-white ' : 'hidden'}`}>
+              <div className='mt-20'>
+                You must login to post an Article
+                <div className='flex justify-center mt-20'>
+                  <LoginButton />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={`${!sessionToken ? 'hidden' : 'block'}`}>
+            <div className='flex justify-center'>
+              <div className='pt-20'>
+              <Title text={title} size={'text-6xl'} />
+                <div className='flex justify-center mt-20'>
+                  <div className='rounded-full focus:outline-none
               bg-light-orange hover:bg-light-white  
               text-light-white dark:bg-dark-orange dark:text-white 
                 w-[100px] py-2 px-4 text-xl font-bold cursor-pointer z-0 absolute content-center'>
-              <div className='flex justify-center'>
-                <BsCardImage size={'26px'} />
-              </div>
-            </div>
-            <input type='file' className='opacity-0 z-10 w-[100px] h-[50px] cursor-pointer' onChange={handleImageUpload} />
-          </div>
-          <div className={`${previewImage === undefined ? 'block mt-2' : 'hidden'}`}>
-            <p className={`${darkMode && 'text-white'} flex justify-center`}>
-              <RiQuillPenLine size={'26px'} /> <span className='pl-3 select-none'>Please upload a cover image</span>
-            </p>
-          </div>
-          <div className='py-5'>
-            <InputField
-              required={true}
-              type='input'
-              placeholder='Title'
-              defaultValue={''}
-              onChange={e => handleTitle(e.target.value)}
-            />
-            <div className={`${title === '' ? 'block mt-2' : 'hidden'}`}>
-              <p className={`${darkMode && 'text-white'} flex justify-center`}>
-                <RiQuillPenLine size={'26px'} /> <span className='pl-3 select-none'>Please enter a title</span>
-              </p>
-            </div>
-            <div className='py-5' />
-            <Editor setContent={setContent} />
-            {/* Editor leaves empty <p> tags if content was added then deleted */}
-            <div className={`${content === '' ? 'block mt-2' : content === '<p></p>' ? 'block mt-2' : 'hidden'}`}>
-              <p className={`${darkMode && 'text-white'} flex justify-center`}>
-                <RiQuillPenLine size={'26px'} /> <span className='pl-3 select-none'>Please enter content</span>
-              </p>
-            </div>
-            <div className='py-5' />
-            <InputField
-              id='tag field'
-              required={true}
-              type='input'
-              placeholder='Tags'
-              value={tagInput}
-              onChange={(e) => {
-                const { value } = e.target
-                setTagInput(value)
-              }}
-              onKeyDown={(e) => writeTag(e)}
-            />
-            <div className='flex justify-center pt-5'>
-              {tags.map((tag, index) =>
-                <div className='px-2'>
-                  <TagIcon tag={tag} index={index} func={() => deleteTag(index)} tagIcon={<MdOutlineCancel />} />
-                </div>)}
-            </div>
-            <div className='flex justify-center dark:text-white pt-5'>
-              <div className={`${tagInput.length > 0 && tagTooShort ? 'block' : tagTooLong ? 'block' : 'hidden'}`}>
-                Tags must be between 3-10 characters
-              </div>
-              <div className={`${tags.length > 0 && !enoughTags ? 'block' : 'hidden'}`}>
-                Add at least 2 tags
-              </div>
-              <div className={`${tooManyTags ? 'block' : 'hidden'}`}>
-                Too many tags!
-              </div>
+                    <div className='flex justify-center'>
+                      <BsCardImage size={'26px'} />
+                    </div>
+                  </div>
+                  <input type='file' className='opacity-0 z-10 w-[100px] h-[50px] cursor-pointer' onChange={handleImageUpload} />
+                </div>
+                <div className={`${previewImage === undefined ? 'block mt-2' : 'hidden'}`}>
+                  <p className={`${darkMode && 'text-white'} flex justify-center`}>
+                    <RiQuillPenLine size={'26px'} /> <span className='pl-3 select-none'>Please upload a cover image</span>
+                  </p>
+                </div>
+                <div className='py-5'>
+                  <InputField
+                    required={true}
+                    type='input'
+                    placeholder='Title'
+                    defaultValue={''}
+                    onChange={e => handleTitle(e.target.value)}
+                  />
+                  <div className={`${title === '' ? 'block mt-2' : 'hidden'}`}>
+                    <p className={`${darkMode && 'text-white'} flex justify-center`}>
+                      <RiQuillPenLine size={'26px'} /> <span className='pl-3 select-none'>Please enter a title</span>
+                    </p>
+                  </div>
+                  <div className='py-5' />
+                  <Editor setContent={setContent} />
+                  {/* Editor leaves empty <p> tags if content was added then deleted */}
+                  <div className={`${content === '' ? 'block mt-2' : content === '<p></p>' ? 'block mt-2' : 'hidden'}`}>
+                    <p className={`${darkMode && 'text-white'} flex justify-center`}>
+                      <RiQuillPenLine size={'26px'} /> <span className='pl-3 select-none'>Please enter content</span>
+                    </p>
+                  </div>
+                  <div className='py-5' />
+                  <InputField
+                    id='tag field'
+                    required={true}
+                    type='input'
+                    placeholder='Tags'
+                    value={tagInput}
+                    onChange={(e) => {
+                      const { value } = e.target
+                      setTagInput(value)
+                    }}
+                    onKeyDown={(e) => writeTag(e)}
+                  />
+                  <div className='flex justify-center pt-5'>
+                    {tags.map((tag, index) =>
+                      <div className='px-2'>
+                        <TagIcon tag={tag} index={index} func={() => deleteTag(index)} tagIcon={<MdOutlineCancel />} />
+                      </div>)}
+                  </div>
+                  <div className='flex justify-center dark:text-white pt-5'>
+                    <div className={`${tagInput.length > 0 && tagTooShort ? 'block' : tagTooLong ? 'block' : 'hidden'}`}>
+                      Tags must be between 3-10 characters
+                    </div>
+                    <div className={`${tags.length > 0 && !enoughTags ? 'block' : 'hidden'}`}>
+                      Add at least 2 tags
+                    </div>
+                    <div className={`${tooManyTags ? 'block' : 'hidden'}`}>
+                      Too many tags!
+                    </div>
 
-            </div>
-            <div className={`${tags.length === 0 ? 'block mt-2' : 'hidden'}`}>
-              <p className={`${darkMode && 'text-white'} flex justify-center`}>
-                <RiQuillPenLine size={'26px'} /> <span className='pl-3 select-none'>Add up to 5 tags. Seperate them with a comma.</span>
-              </p>
-            </div>
-            <div className='py-5' />
-            <div className='flex justify-center'>
-              <Button label={'Submit'} func={handleSubmit} />
-            </div>
-            <div className='flex justify-center pb-20 sm:pb-10'>
-              <p className='text-center mt-2 text-black dark:text-white select-none'>Articles must comply with the terms of service. Find out more in the <Link to="/articleguide" className="font-medium text-light-orange dark:text-dark-orange" style={{ textDecoration: 'none' }}>Article Guide</Link>.</p>
+                  </div>
+                  <div className={`${tags.length === 0 ? 'block mt-2' : 'hidden'}`}>
+                    <p className={`${darkMode && 'text-white'} flex justify-center`}>
+                      <RiQuillPenLine size={'26px'} /> <span className='pl-3 select-none'>Add up to 5 tags. Seperate them with a comma.</span>
+                    </p>
+                  </div>
+                  <div className='py-5' />
+                  <div className='flex justify-center'>
+                    <Button label={'Submit'} func={handleSubmit} />
+                  </div>
+                  <div className='flex justify-center pb-20 sm:pb-10'>
+                    <p className='text-center mt-2 text-black dark:text-white select-none'>Articles must comply with the terms of service. Find out more in the <Link to="/articleguide" className="font-medium text-light-orange dark:text-dark-orange" style={{ textDecoration: 'none' }}>Article Guide</Link>.</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-}
+      }
     </>
   )
 }
