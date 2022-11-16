7ad0e0f9d2f1bbd9c1fae68ede5a6189a1de69bf
anthonygavriel <anthonygavriel@gmail.com>
2022-11-16 17:30:59 +0000

    docs changes

--- frontend/src/pages/user/UserProfile.jsx
+++ frontend/src/pages/user/UserProfile.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import API from '../../API'
 import parser from 'html-react-parser'
-import { Header, SidebarV2, Title, Button, LogoutButton, ProfileArticleBar } from '../../components'
+import { Header, SidebarV2, Title, Button, LogoutButton, ProfileArticleBar, ArticleLoading } from '../../components'
 import { useStateContext } from '../../context/ContextProvider'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { useParams } from 'react-router-dom'
 
@@ -19,7 +19,6 @@ import { useParams } from 'react-router-dom'
  * 
  * TODO: Add a list of likes, posts etc
  * TODO: Recreate a MiniArticle component for UserProfile page
- * TODO: Fix the display_name bug
  */
 
 
@@ -32,9 +31,9 @@ const UserProfile = () => {
     username: '',
     bio: '',
     profile_image: '',
-    display_name: '',
     authored: [],
   })
+  const [notLoaded, setNotLoaded] = useState(false)
 
   useEffect(() => {
     const profileDetail = () => {
@@ -42,57 +41,68 @@ const UserProfile = () => {
         .then((res) => {
           setProfileData(res.data)
           console.log(res.data)
-          // if (res.data.displayName.length > 20) {
-          //   setDisplayName(res.data.display_name.slice(0, 20) + '...')
-          // } else {
-          //   setDisplayName(res.data.display_name)
-          // }
-        }).catch(console.err)
+        }).catch(err => {
+          console.log(err.response)
+          setNotLoaded(true)
+        })
     }
     profileDetail()
   }, [id])
-
-  return (
-    <>
-      <div className='fixed justify-center m-auto left-0 right-0'>
-        <SidebarV2 />
-        <ProfileArticleBar header={'Articles Written'} articles={profileData.authored}/>
-        <Header page={'user'} />
-      </div>
-      <div className={`${profileData.profile_name === null ? 'block' : 'hidden'} pt-20 flex justify-center dark:text-white`}>
-        <Title text={profileData.username} lengthLimit={true}/>
-      </div>
-      <div className={`${profileData.profile_name === null ? 'hidden' : 'block'} pt-20 flex justify-center dark:text-white`}>
-        <Title text={profileData.profile_name} size={'text-3xl'}/>
-      </div>
-
-
-    <div className='grid grid-cols-1'>
-
-    <div className='mt-10 flex justify-center'>
-        <img src={profileData.profile_image} className='rounded-lg' alt='' height='200px' width='200px' />
-      </div>
-      <div className={`${profileData.bio === null ? 'hidden' : 'justify-center mt-10 w-[200px] m-auto dark:text-white'}`}>
-        {parser(String(profileData.bio))}
-      </div>
-    </div>
-      <div className={`${id === loggedInProfile.id ? 'flex justify-center mt-10' : 'hidden'}`}>
-        {/* If logged in profile ID is equal to router URL ID && sessionToken then show */}
-        <div className={`px-2 ${profileData.id === loggedInProfile.id ? 'block px-2' : 'hidden'}`}>
-          <Link to={'/profiles/edit'} replace={true}>
-            <Button icon={<AiOutlineEdit size={'26px'} />} label={'Edit profile'} labelProps={'text-sm pt-1 pl-2'} />
-          </Link>
+  if (profileData.id === '') {
+    return (
+      <>
+        <ArticleLoading pageTitle={'Loading'} />
+        {
+          notLoaded && (
+            <Navigate to={'not-found'} replace={true} />
+          )
+        }
+      </>
+    )
+  }
+  else {
+    return (
+      <>
+        <div className='fixed justify-center m-auto left-0 right-0'>
+          <SidebarV2 />
+          <ProfileArticleBar header={'Articles Written'} articles={profileData.authored} />
+          <Header page={'user'} />
         </div>
-        <div className={`px-2 ${profileData.id === loggedInProfile.id ? 'block px-2' : 'hidden'}`}>
-          <LogoutButton />
+        <div className={`${profileData.profile_name === null ? 'block' : 'hidden'} pt-20 flex justify-center dark:text-white`}>
+          <Title text={profileData.username} lengthLimit={true} />
         </div>
-      </div>
-      <div className={`${profileData.id !== loggedInProfile.id ? 'block' : 'hidden'}`}>
-        <Button label={'Tip'} />
-      </div>
-    </>
+        <div className={`${profileData.profile_name === null ? 'hidden' : 'block'} pt-20 flex justify-center dark:text-white`}>
+          <Title text={profileData.profile_name} size={'text-3xl'} />
+        </div>
 
-  )
+
+        <div className='grid grid-cols-1'>
+
+          <div className='mt-10 flex justify-center'>
+            <img src={profileData.profile_image} className='rounded-lg' alt='' height='200px' width='200px' />
+          </div>
+          <div className={`${profileData.bio === null ? 'hidden' : 'justify-center mt-10 w-[200px] m-auto dark:text-white'}`}>
+            {parser(String(profileData.bio))}
+          </div>
+        </div>
+        <div className={`${id === loggedInProfile.id ? 'flex justify-center mt-10' : 'hidden'}`}>
+          {/* If logged in profile ID is equal to router URL ID && sessionToken then show */}
+          <div className={`px-2 ${profileData.id === loggedInProfile.id ? 'block px-2' : 'hidden'}`}>
+            <Link to={'/profiles/edit'} replace={true}>
+              <Button icon={<AiOutlineEdit size={'26px'} />} label={'Edit profile'} labelProps={'text-sm pt-1 pl-2'} />
+            </Link>
+          </div>
+          <div className={`px-2 ${profileData.id === loggedInProfile.id ? 'block px-2' : 'hidden'}`}>
+            <LogoutButton />
+          </div>
+        </div>
+        <div className={`${profileData.id !== loggedInProfile.id ? 'block' : 'hidden'}`}>
+          <Button label={'Tip'} />
+        </div>
+      </>
+
+    )
+  }
 }
 
 export default UserProfile
