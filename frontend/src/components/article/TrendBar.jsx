d3ad68fa00eb1fcfd355bb27ee6e262b39299cfe
anthonygavriel <anthonygavriel@gmail.com>
2022-11-22 21:23:34 +0000

    TrendBar shows Articles with likes ascending

--- frontend/src/components/article/TrendBar.jsx
+++ frontend/src/components/article/TrendBar.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useStateContext } from '../../context/ContextProvider'
 import Title from '../misc/Title'
 import TrendCard from './TrendCard'
 import { Link } from 'react-router-dom'
+import API from '../../API'
 
 /**
  * Bar designed to auto-update showing trending items in descending order
@@ -12,12 +13,26 @@ import { Link } from 'react-router-dom'
  */
 
 const TrendBar = () => {
-    const { articleList } = useStateContext()
+    const [mostPopular, setMostPopular] = useState([])
+
+    useEffect(() => {
+        const getPopular = async () => {
+            await API.get('/articles/popular')
+                .then((res) => {
+                    console.log(res.data)
+                    setMostPopular(res.data)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+
+        getPopular()
+    }, [])
 
     return (
         <div>
             <div className={`flex invisible xl:visible items-end px-10
-            }
             flex-col h-full 
             border-light-orange dark:border-dark-orange border-opacity-50`}>
                 <div className='duration-300 h-screen bg-opacity-5 dark:bg-opacity-5 overflow-scroll fixed'>
@@ -26,42 +41,18 @@ const TrendBar = () => {
                         <Title text={'trending'} size={'text-3xl'} />
                     </div>
                     <div className='pt-4 grid grid-cols-1 gap-y-4'>
-                        <Link to={`/articles/${articleList?.[0]?.id}`} style={{ textDecoration: 'none' }}>
-                            <TrendCard
-                                id={articleList?.[0]?.id}
-                                // image={articles?.[0].image}
-                                // imageAlt={articles?.[0].imageAlt}
-                                title={articleList?.[0]?.title}
-                                tags={articleList?.[0]?.tags}
-                            />
-                        </Link>
-                        <Link to={`/articles/${articleList?.[1]?.id}`} style={{ textDecoration: 'none' }}>
-                            <TrendCard
-                                id={articleList?.[1]?.id}
-                                // image={articles?.[3].image}
-                                // imageAlt={articles?.[3].imageAlt}
-                                title={articleList?.[1]?.title}
-                                tags={articleList?.[1]?.tags}
-                            />
-                        </Link>
-                        <Link to={`/articles/${articleList?.[2]?.id}`} style={{ textDecoration: 'none' }}>
-                            <TrendCard
-                                id={articleList?.[2]?.id}
-                                // image={articles?.[3].image}
-                                // imageAlt={articles?.[3].imageAlt}
-                                title={articleList?.[2]?.title}
-                                tags={articleList?.[2]?.tags}
-                            />
-                        </Link>
-                        <Link to={`/articles/${articleList?.[3]?.id}`} style={{ textDecoration: 'none' }}>
-                            <TrendCard
-                                id={articleList?.[3]?.id}
-                                // image={articles?.[3].image}
-                                // imageAlt={articles?.[3].imageAlt}
-                                title={articleList?.[3]?.title}
-                                tags={articleList?.[3]?.tags}
-                            />
-                        </Link>
+                        {mostPopular.map((article) => (
+                            <div className='pt-4 grid grid-cols-1 gap-y-4' key={article.id}>
+                                {console.log(article.id)}
+                                <Link to={`/articles/${article.id}`} style={{textDecoration: 'none'}}>
+                                    <TrendCard
+                                        id={article.id}
+                                        title={article.title}
+                                        tags={article.tags}
+                                    />
+                                </Link>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
