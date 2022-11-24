44416fc575c01446b409d11283cc5d0fce54bec3
anthonygavriel <anthonygavriel@gmail.com>
2022-11-24 17:43:58 +0000

    shows all content related to user

--- frontend/src/pages/user/UserProfile.jsx
+++ frontend/src/pages/user/UserProfile.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import API from '../../API'
 import parser from 'html-react-parser'
-import { Header, SidebarV2, Title, Button, LogoutButton, ProfileArticleBar, ArticleLoading } from '../../components'
+import { Header, SidebarV2, Title, Button, LogoutButton, ProfileArticleBar, ArticleLoading, ProfileArticle } from '../../components'
 import { useStateContext } from '../../context/ContextProvider'
 import { Link, Navigate } from 'react-router-dom'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { useParams } from 'react-router-dom'
+import { BiLike, BiDislike } from 'react-icons/bi'
+import { BsPen } from 'react-icons/bs'
 
 /**
  * Page which displays information about the user
@@ -32,14 +34,18 @@ const UserProfile = () => {
     bio: '',
     profile_image: '',
     authored: [],
+    reacted: [],
   })
   const [notLoaded, setNotLoaded] = useState(false)
+  const [showArticles, setShowArticles] = useState([])
+  const [title, setTitle] = useState('Articles Written')
 
   useEffect(() => {
     const profileDetail = () => {
       API.get(`/profile/user/${id}/`)
         .then((res) => {
           setProfileData(res.data)
+          setShowArticles(res.data.authored)
           console.log(res.data)
         }).catch(err => {
           console.log(err.response)
@@ -48,6 +54,34 @@ const UserProfile = () => {
     }
     profileDetail()
   }, [id])
+
+  const sortByWritten = () => {
+    setTitle('Articles Written')
+    setShowArticles(profileData.authored)
+  }
+
+  const sortByLiked = () => {
+    setTitle('Articles Liked')
+    const liked = []
+    for(var i = 0; i < profileData.reacted.length; i++) {
+      if(profileData.reacted[i].sentiment === 1) {
+        liked.push(profileData.reacted[i].article_id)
+      } 
+    }
+    setShowArticles(liked)
+  }
+
+  const sortByDisliked = () => {
+    const disliked = []
+    setTitle('Articles Disliked')
+    for(var i = 0; i < profileData.reacted.length; i++) {
+      if(profileData.reacted[i].sentiment === 2) {
+        disliked.push(profileData.reacted[i].article_id)
+      } 
+    }
+    setShowArticles(disliked)
+  }
+
   if (profileData.id === '') {
     return (
       <>
@@ -64,9 +98,38 @@ const UserProfile = () => {
     return (
       <>
         <div className='fixed justify-center m-auto left-0 right-0'>
-          <SidebarV2 />
-          <ProfileArticleBar header={'Articles Written'} articles={profileData.authored} />
+          {/* <ProfileArticleBar header={'Articles Written'} articles={profileData.authored} /> */}
+          <div>
+            <div className={`flex invisible xl:visible items-end px-10 flex-col h-full`}>
+                <div className='duration-300 h-screen bg-opacity-5 dark:bg-opacity-5 overflow-scroll fixed'>
+                    <div className='py-4' />
+                    <div className='w-[300px] h-[10px] justify-center py-10'>
+                        <Title text={title} size={'text-3xl'} />
+                    </div>
+                    {showArticles.map((article) => (
+                        <div className='pt-4 grid grid-cols-1 gap-y-4' key={article.id}>
+                            <ProfileArticle
+                                id={article.id}
+                                title={article.title}
+                                image={article.preview_image}
+                                imageHeight={80}
+                                imageWidth={80}
+                            />
+                        </div>
+                    ))}
+
+                    <div className='flex justify-center space-x-2 mt-2'>
+                        <Button icon={<BsPen size={'26px'} />} func={() => sortByWritten()}/>
+                        <Button icon={<BiLike size={'26px'} />} func={() => sortByLiked()}/>
+                        <Button icon={<BiDislike size={'26px'} />} func={() => sortByDisliked()}/>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+          {/* <ProfileArticleBar header={'Articles Written'} articles={showArticles} /> */}
           <Header page={'user'} />
+          <SidebarV2 />
         </div>
         <div className={`${profileData.profile_name === null ? 'block' : 'hidden'} pt-20 flex justify-center dark:text-white`}>
           <Title text={profileData.username} lengthLimit={true} />
@@ -74,7 +137,6 @@ const UserProfile = () => {
         <div className={`${profileData.profile_name === null ? 'hidden' : 'block'} pt-20 flex justify-center dark:text-white`}>
           <Title text={profileData.profile_name} size={'text-3xl'} />
         </div>
-
 
         <div className='grid grid-cols-1'>
 
