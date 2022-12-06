9ddb414f3493d5d6f93214326cb88d65fddf02a3
anthonygavriel <anthonygavriel@gmail.com>
2022-12-06 23:09:00 +0000

    tip button added

--- frontend/src/components/article/AuthorBar.jsx
+++ frontend/src/components/article/AuthorBar.jsx
@@ -3,13 +3,20 @@ import Title from '../misc/Title'
 import Button from '../misc/Button'
 import { Link } from 'react-router-dom'
 import API from '../../API'
+import TipButton from '../user/TipButton'
+
+/**
+ * 
+ * @param {String} authorID - ID of the Author  
+ * @returns Link showing display name, profile picture and a tip button of article author
+ */
 
 const AuthorBar = ({ authorID }) => {
   const [authorProfile, setAuthorProfile] = useState(undefined)
 
   useEffect(() => {
     const authorDetail = async () => {
-      await API.get(`/profile/user/${authorID}`)
+      await API.get(`/profile/user/author/${authorID}`)
         .then((res) => {
           setAuthorProfile(res.data)
         })
@@ -19,6 +26,21 @@ const AuthorBar = ({ authorID }) => {
     }
     authorDetail()
   }, [authorID])
+
+  const checkProfileName = () => {
+    if(authorProfile?.profile_name === '') {
+      setAuthorProfile({
+        id: authorProfile.id,
+        username: authorProfile.username,
+        profile_name: authorProfile.username.slice(0,6) + '...',
+        profile_image: authorProfile.profile_image
+      })
+    }
+
+    return (
+      <Title text={authorProfile?.profile_name} size={'text-3xl'} lengthLimit={true} />
+    )
+  }
 
   return (
     <div>
@@ -30,7 +52,7 @@ const AuthorBar = ({ authorID }) => {
             <Link to={`/profiles/${authorID}`} style={{ textDecoration: 'none' }}>
               <div className='grid grid-cols-1 gap-y-4 content-center justify-center text-center m-auto'>
                 <div>
-                  <Title text={authorProfile?.profile_name} size={'text-3xl'} lengthLimit={true} />
+                {checkProfileName()}
                 </div>
                 <div className='flex justify-center'>
                   <img src={authorProfile?.profile_image} alt={'Authors profile'} width={120} />
@@ -39,7 +61,7 @@ const AuthorBar = ({ authorID }) => {
             </Link>
           </div>
           <div className='flex justify-center mb-2'>
-            <Button label={'Tip ₳'} />
+            <TipButton authorUsername={authorProfile?.username} amountInADA={1}/>
           </div>
         </div>
       </div>
