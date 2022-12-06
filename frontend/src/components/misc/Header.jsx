92e9b6a6309e959d4510d63712372697399fd654
anthonygavriel <anthonygavriel@gmail.com>
2022-12-06 23:12:27 +0000

    style fix

--- frontend/src/components/misc/Header.jsx
+++ frontend/src/components/misc/Header.jsx
@@ -34,8 +34,8 @@ const Header = ({ page }) => {
     return (
         <div>
             <div className='flex items-end flex-col'>
-                <div className='flex min-w-full sm:w-1/2 items-center justify-center bottom-0 sm:left-24 sm:justify-end sm:top-0 sm:h-16 fixed sm:px-20 sm:py-1 sm:dark:bg-opacity-0 bg-white dark:bg-dark-grey'>
-                    <div className={`flex flex-row justify-end pr-10 w-full ${page === 'landing' ? 'bg-opacity-0' : 'bg-opacity-100'}`}>
+                <div className={`flex min-w-full sm:w-1/2 items-center justify-center bottom-0 sm:left-24 sm:justify-end sm:top-0 sm:h-16 fixed sm:px-20 sm:py-1 sm:dark:bg-opacity-0 bg-white dark:bg-dark-grey ${page === 'landing' ? 'bg-opacity-0' : 'bg-opacity-100'}`}>
+                    <div className={`flex flex-row justify-end pr-10 w-full ${page === 'landing' ? 'bg-opacity-0' : 'bg-opacity-100 dark:bg-dark-grey'}`}>
                         <div className={`${page === 'home' ? 'block py-3 px-4' : 'hidden'} ${page === 'landing' && 'hidden'} ${page === 'login' && 'hidden'}`}>
                             <RefreshArticles />
                         </div>
