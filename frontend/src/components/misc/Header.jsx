47da784ed013a5efccf66d56ab5ed686d81e6c2e
anthonygavriel <anthonygavriel@gmail.com>
2022-12-16 01:04:44 +0000

    notification button added

--- frontend/src/components/misc/Header.jsx
+++ frontend/src/components/misc/Header.jsx
@@ -5,7 +5,7 @@ import { useStateContext } from '../../context/ContextProvider'
 import { BiTrendingUp } from 'react-icons/bi'
 import { useNavigate } from 'react-router-dom'
 import RefreshArticles from '../article/RefreshArticles'
-import { Button, LoginButton, SortingButton } from '..'
+import { Button, LoginButton, SortingButton, NotificationsButton } from '..'
 import { IoArrowBack } from 'react-icons/io5'
 
 /**
@@ -50,11 +50,14 @@ const Header = ({ page }) => {
                                 />
                             </Link>
                         </div>
-                        <div className={`${page === 'edit' ? 'block pr-2 py-3' : page === 'articleDetail' ? 'block pr-2 py-3' : page === 'login' ? 'block pr-2 py-3' : 'hidden'}`}>
+                        <div className={`${page === 'edit' ? 'block pr-2 py-3' : page === 'articleDetail' ? 'block pr-2 py-3' : page === 'login' ? 'block pr-2 py-3' : page === 'thread' ? 'block pr-2 py-3' : 'hidden'}`}>
                             <Button label={'Back'} labelProps={'text-sm pt-1 pl-2'} icon={<IoArrowBack size={'26px'} />} func={() => history(-1)} />
                         </div>
                         <div className={`${page === 'landing' ? 'hidden' : page === 'login' ? 'hidden' : page === 'user' ? 'hidden' : 'pr-2 py-3'}`}>
                             <LoginButton />
+                        </div>
+                        <div className={`${page === 'landing' ? 'hidden' : page === 'login' ? 'hidden' : page === 'user' ? 'hidden' : 'block pr-2 py-3'}`}>
+                            <NotificationsButton />
                         </div>
                         <div className={`${page !== 'landing' && 'hidden'} py-3 ${!showSkipButton && 'hidden'}`}>
                             <Link to={'/home'}>
