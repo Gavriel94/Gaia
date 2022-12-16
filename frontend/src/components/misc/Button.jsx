4c7f5ede0d72d09b56ea2d43fdc1b810425d57d6
anthonygavriel <anthonygavriel@gmail.com>
2022-12-16 01:04:35 +0000

    new notification animation added

--- frontend/src/components/misc/Button.jsx
+++ frontend/src/components/misc/Button.jsx
@@ -11,11 +11,12 @@ import React from 'react'
  * @param {string} imageAlt - Alt text required for images
  * @param {string} imageWidth - Set the image width in px
  * @param {string} imageHeight - Set the image height in px
+ * @param {string} notification - Set to show a pusling animation if there is a notification
  * 
  * @returns {JSX.Element} Functional button with the consistent styling of Gaia
  */
 
-const Button = ({ label, labelProps, func, icon, image, imageAlt, imageWidth, imageHeight }) => {
+const Button = ({ label, labelProps, func, icon, image, imageAlt, imageWidth, imageHeight, notification }) => {
     return (
         <button
             type='button'
@@ -24,10 +25,13 @@ const Button = ({ label, labelProps, func, icon, image, imageAlt, imageWidth, im
         bg-light-orange hover:bg-light-white hover:text-light-orange text-light-white
         dark:bg-dark-orange dark:hover:bg-dark-grey dark:hover:text-dark-orange dark:text-white py-2 px-4 text-xl font-bold transition-color duration-500 cursor-pointer'>
             <div className='flex flex-row text-center justify-center'>
-            {<img src={image} alt={imageAlt} height={imageHeight} width={imageWidth} />}
+                {<img src={image} alt={imageAlt} height={imageHeight} width={imageWidth} />}
                 {icon}
                 <div className={`${labelProps}`}>
                     {label}
+                </div>
+                <div className={`${notification === 'true' ? 'block' : 'hidden'}`}>
+                    <div className="animate-ping absolute inline-flex rounded-full h-3 w-3 bg-light-white"></div>
                 </div>
             </div>
         </button>
