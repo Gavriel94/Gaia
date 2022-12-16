df3ecb070a6f5cd19181296bd43333257c28bef5
anthonygavriel <anthonygavriel@gmail.com>
2022-12-16 01:03:17 +0000

    gradient calculated here

--- frontend/src/components/article/SentimentIndicator.jsx
+++ frontend/src/components/article/SentimentIndicator.jsx
@@ -1,23 +1,54 @@
 import React from 'react'
-import { BiLike, BiDislike } from 'react-icons/bi'
 
-const SentimentIndicator = ({ dislikes, likes, likePercent, gradient, miniArticle }) => {
+const SentimentIndicator = ({ dislikes, likes, likePercent }) => {
+
+    const calculateIndicatorGradient = () => {
+        if (likePercent === 100) {
+            const gradient = 'from-light-green to-light-green'
+            return (
+                <div className={`bg-gradient-to-r ${gradient} rounded-lg px-5 select-none text-center text-white`}>
+                    100% likes
+                </div>
+            )
+        } else if (likePercent === 0 && dislikes > 0) {
+            const gradient = 'from-light-red to-light-red'
+            return (
+                <div className={`bg-gradient-to-r ${gradient} rounded-lg px-5 select-none text-center text-white`}>
+                    100% dislikes
+                </div>
+            )
+        } else if (likePercent === 0 && dislikes === 0) {
+            return (
+                <div className='text-black dark:text-white'>
+                    No likes
+                </div>
+            )
+        } else {
+            const gradient = 'from-light-red to-light-green'
+            return (
+                <div className={`bg-gradient-to-r ${gradient} rounded-lg px-5 select-none text-center text-white`}>
+                    {likePercent}% likes
+                </div>
+            )
+        }
+    }
 
     return (
         <div className='flex justify-center mt-5'>
             <div className='flex flex-row'>
-
-            <div className='text-center flex justify-end select-none px-5'>
+                <div className={`${dislikes === 1 ? 'text-center flex px-5 select-none text-black dark:text-white' : 'hidden'}`}>
+                    {dislikes} dislike
+                </div>
+                <div className={`${dislikes !== 1 ? 'text-center flex px-5 select-none text-black dark:text-white' : 'hidden'}`}>
                     {dislikes} dislikes
                 </div>
                 <div>
-                    <div className={`bg-gradient-to-r ${gradient} rounded-lg px-5 select-none text-center`}>
-                        <div className='text-white'>
-                        &nbsp; {likePercent}% likes &nbsp;
-                        </div>
-                    </div> 
+                    {calculateIndicatorGradient()}
                 </div>
-                <div className='text-center flex px-5 select-none'>
+                <div className={`${likes === 1 ? 'text-center flex px-5 select-none text-black dark:text-white' : 'hidden'}`}>
+                    {likes} like
+                </div>
+                <div className={`${likes !== 1 ? 'text-center flex px-5 select-none text-black dark:text-white' : 'hidden'}`}>
                     {likes} likes
                 </div>
             </div>
