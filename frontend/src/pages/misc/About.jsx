d825e367a03b5296cc211a9bcf8147010ec5de9d
anthonygavriel <anthonygavriel@gmail.com>
2023-04-26 00:19:55 +0100

    about page

--- frontend/src/pages/misc/About.jsx
+++ frontend/src/pages/misc/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { SidebarV2, Header, Title, Footer } from '../../components'
+import { Link } from 'react-router-dom'
 
 /**
  * Page to contain information about the project and information about creating wallets
@@ -8,49 +9,55 @@ import { SidebarV2, Header, Title, Footer } from '../../components'
 const About = () => {
     return (
         <>
-            <div className='fixed justify-center m-auto left-0 right-0 '>
-                <Header />
-                <SidebarV2 />
+            <Header />
+            <SidebarV2 />
+            <div className='flex justify-center mt-20'>
+                <Title text={'About Gaia'} size={'text-6xl'} />
             </div>
-            <div className='flex justify-center h-screen'>
-                <div className='pt-20'>
-                    <Title text={'about'} size={'text-6xl'} hover={true} />
-                    <div className='mt-20 overflow-auto text-center dark:text-white'>
-                        {/* <Title text={'Ethos'} size={'text-3xl'} hover={true} /> */}
-                        <div className='overflow-auto text-center dark:text-white'>
-                            Gaia is a social publishing platform developed with a mission to promote informative and responsible content sharing.
-                        </div>
-                        <div className='overflow-auto text-center dark:text-white'>
-                            Built with the Cardano blockchain at its foundation, Gaia ensures a secure, reliable and environmentally-friendly solution for users.
-                        </div>
-                        <div className='overflow-auto text-center dark:text-white'>
-                            Use your Cardano wallet to login.
-                            <br />
-                            Wallet logins ensure user privacy while encouraging open and honest discussions on various topics.
-                        </div>
-                        <div className='overflow-auto text-center dark:text-white'>
-                            By utilising advanced artificial intelligence, Gaia actively combats hate speech and illegal material creating a safe and inclusive space for users of all ages.
-                        </div>
-                        <div className='overflow-auto text-center dark:text-white'>
-                            Use the Sentiment Indicator for an at-a-glance way to see how the community feel about an article.
-                            <br />
-                            Express your feelings towards articles by using the 'Like' or 'Dislike' buttons and help shape the platform.
-                        </div>
-                        <div className='overflow-auto text-center dark:text-white'>
-                            Topics are a quick and easy way for you to find your interests and have discussions.
-                        </div>
-                        <div className='overflow-auto text-center dark:text-white'>
-                            As you're logged in with your wallet you can send tips in ADA to other users if you like their articles!
-                            <br />
-                            Cardano's transaction system ensures you are not charged if a transaction fails.
-                        </div>
-                        <div className='overflow-auto text-center dark:text-white'>
-                            Join Gaia to be part of an innovative community that values information sharing in a balanced ecosystem.
-                        </div>
-                    </div>
+            <div className='justify-center m-auto text-center dark:text-white w-1/2 mt-20'>
+                <div className='mt-5'>
+                    Gaia is a social publishing platform developed with a mission to promote informative and responsible content sharing.
+                </div>
+                <div className='mt-5'>
+                    Built with the <a href="https://cardano.org">Cardano</a> blockchain at its foundation, Gaia ensures a secure, reliable and environmentally-friendly platform for users.
+                </div>
+                <div className='mt-5'>
+                    Use your Cardano wallet to login or learn how to create one <Link to='/walletinstructions' style={{
+                        textDec
+                            : 'none'
+                    }}>here</Link>
+                </div>
+                <div className='mt-5'>
+                    Wallet logins ensure user privacy while encouraging open and honest discussions on various topics.
+                </div>
+                <div className='mt-5'>
+                    By utilising advanced artificial intelligence, Gaia actively combats hate speech and illegal material creating a safe and inclusive space for users of all ages.
+                </div>
+                <div className='mt-5'>
+                    Use the Sentiment Indicator for an at-a-glance way to see how the community feel about an article
+                </div>
+                <div className='mt-5'>
+                    Express your feelings towards articles by using the 'Like' or 'Dislike' buttons and help shape the platform.
+                </div>
+                <div className='mt-5'>
+                    Topics are a quick and easy way for you to find your interests and have discussions. Find what you're looking for in the search page or create it if it doesn't exist!
+                </div>
+                <div className='mt-5'>
+                    As you're logged in with your wallet you can send tips in ADA to other users if you like their articles
+                </div>
+                <div className='mt-5'>
+                    Cardano's transaction system ensures you are not charged if a transaction fails.
+                </div>
+                <div className='mt-5'>
+                    Gaia has no access to your funds at any time and never takes a fee
+                </div>
+                <div className='mt-5'>
+                    Join Gaia to be part of an innovative community that values information sharing in a balanced ecosystem.
                 </div>
             </div>
-            <Footer />
+            <div className='fixed bottom-0 w-full justify-center'>
+                <Footer />
+            </div>
         </>
     )
 }
